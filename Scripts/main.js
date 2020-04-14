fad9b4f94e085066b546021ba82ee066b070362b
Parth Mehta <53347017+debparth@users.noreply.github.com>
2020-04-14 05:42:47 -0700

    Add files via upload

---
+++ Scripts/main.js
@@ -0,0 +1,44 @@
+// Code goes here
+$(document).ready(function() {
+    var stockTable = $('#table1').dataTable({
+        "sPaginationType": "full_numbers",
+        "bFilter": false
+    }); // first table 
+    
+	var catalogTable = $('#table2').dataTable({
+        "sPaginationType": "full_numbers",
+        "bFilter": false
+    }); // Second table 
+   
+    stockTable.on('click', 'tbody tr' ,function() {
+		 $(this).toggleClass('selected');
+    });
+	catalogTable.on('click', 'tbody tr' ,function() {
+		 $(this).toggleClass('selected');
+    });
+	
+	$('#LeftMove').on('click',function () {
+		moveRows(catalogTable, stockTable);
+	});
+	
+	$('#RightMove').on('click',function () {
+		moveRows(stockTable, catalogTable);
+	});
+	
+});
+	
+function moveRows(fromTable, toTable){
+	var $row= fromTable.find(".selected");
+	$.each($row, function(k, v){
+		if(this !== null){
+			addRow = fromTable.fnGetData(this);
+			toTable.fnAddData(addRow);
+			fromTable.fnDeleteRow(this);
+		}
+	});
+}
+
+function darkmode() {
+	var element = document.body;
+	element.classList.toggle("dark-mode");
+ }
